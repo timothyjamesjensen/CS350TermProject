95d64663601309ea490eea226b21a725dd9d50d7
Timothy Jensen <timothyjamesjensen@gmail.com>
2015-03-05 22:52:08 -0800

    added elementary class for elementary multiplication

--- src/main/java/com/jensen/algorithms/Karatsuba.java
+++ src/main/java/com/jensen/algorithms/Karatsuba.java
@@ -1,21 +1,18 @@
 package com.jensen.algorithms;
 
 import java.math.BigInteger;
-import java.util.Random;
+import java.util.Arrays;
 
 /**
  * Created by tjjensenjr on 3/4/15.
  */
 public class Karatsuba {
-    Random rnd = new Random();
-    BigInteger bigInt = new BigInteger(300, rnd);
-
     public static BigInteger algorithm(BigInteger x, BigInteger y) {
         int N = Math.max(x.bitLength(), y.bitLength());
 
         if (N <= 1) return x.multiply(y);
 
-        N = (N/2) + (N%2);
+        N = (N / 2) + (N % 2);
 
         BigInteger b = x.shiftRight(N);
         BigInteger a = x.subtract(b.shiftLeft(N));
@@ -26,6 +23,7 @@ public class Karatsuba {
         BigInteger bd = Karatsuba.algorithm(b, d);
         BigInteger abcd = Karatsuba.algorithm(a.add(b), c.add(d));
 
-        return ac.add(abcd.subtract(ac).subtract(bd).shiftLeft(N)).add(bd.shiftLeft(2*N));
+        return ac.add(abcd.subtract(ac).subtract(bd).shiftLeft(N)).add(bd.shiftLeft(2 * N));
     }
+
 }
--- src/main/java/com/jensen/driver/driver.java
+++ src/main/java/com/jensen/driver/driver.java
@@ -1,5 +1,6 @@
 package com.jensen.driver;
 
+import com.jensen.algorithms.Elementary;
 import com.jensen.algorithms.Karatsuba;
 
 import java.math.BigInteger;
@@ -11,25 +12,35 @@ import java.util.Random;
 public class driver {
     public static void main(String[] args) {
         Random rnd = new Random();
-        final BigInteger x = new BigInteger(300, rnd);
-        final BigInteger y = new BigInteger(300, rnd);
+        int bitSize = 10;
+        final BigInteger x = new BigInteger(bitSize, rnd);
+        final BigInteger y = new BigInteger(bitSize, rnd);
+        BigInteger result = new BigInteger("0");
+        BigInteger a = new BigInteger("10");
+        Elementary elementary = new Elementary();
 
         final Karatsuba karatsuba = new Karatsuba();
-        BigInteger result = new BigInteger("0");
 
+        long executionTime = 0;
         long startTime = 0;
         long endTime = 0;
 
         startTime = System.nanoTime();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100; i++) {
+            BigInteger n = new BigInteger(elementary.multiply(x.toString(), y.toString()));
+        }
+        endTime = System.nanoTime();
+
+        executionTime = (endTime - startTime) / 100;
+        System.out.println(executionTime);
+
+        startTime = System.nanoTime();
+        for (int i = 0; i < 100; i++) {
             result = karatsuba.algorithm(x, y);
         }
         endTime = System.nanoTime();
 
-        long executionTime = (endTime - startTime) / 1000;
-
+        executionTime = (endTime - startTime) / 100;
         System.out.println(executionTime);
-        System.out.println(result);
-        System.out.println(x.multiply(y));
     }
 }
