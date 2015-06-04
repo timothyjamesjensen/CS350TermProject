fee35aaf3ef10b25294979842b2aed1378b83d28
Timothy Jensen <timothyjamesjensen@gmail.com>
2015-06-03 17:53:53 -0700

    created fake method

--- src/main/java/com/jensen/algorithms/fakeClass.java
+++ src/main/java/com/jensen/algorithms/fakeClass.java
@@ -4,4 +4,8 @@ package com.jensen.algorithms;
  * Created by tjjensenjr on 6/3/15.
  */
 public class fakeClass {
+
+    private static void printer() {
+        System.out.println("fake print");
+    }
 }
