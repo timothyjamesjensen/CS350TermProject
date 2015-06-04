c95341b0886ed6081da8c9d72537b5a01e827ed9
Timothy Jensen <timothyjamesjensen@gmail.com>
2015-06-03 18:07:10 -0700

    Created a printer function

--- src/main/java/com/jensen/algorithms/PracticeClass.java
+++ src/main/java/com/jensen/algorithms/PracticeClass.java
@@ -4,4 +4,8 @@ package com.jensen.algorithms;
  * Created by tjjensenjr on 6/3/15.
  */
 public class PracticeClass {
+
+    private static void printerFunction() {
+        System.out.println("This is a printer function");
+    }
 }
